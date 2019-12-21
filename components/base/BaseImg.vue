df8c72b227b83eac67712be656d0ab1f36c27d6f
aaron <aaron@mindvalley.com>
2019-12-21 13:04:18 +0800

    adds generic header and footer

---
+++ components/base/BaseImg.vue
@@ -0,0 +1,39 @@
+<template>
+  <img
+    :class="classes"
+    :loading="lazyEnabled"
+    :src="src"
+    alt="Destination Malaysia"
+    class="lazyload"
+  />
+</template>
+
+<script>
+export default {
+  name: 'BaseImg',
+  props: {
+    src: {
+      type: String,
+      default: 'https://via.placeholder.com/300x300.png'
+    },
+    alt: {
+      type: String,
+      default: 'Alt Text'
+    },
+    classes: {
+      type: Array,
+      default: () => [],
+      required: false
+    }
+  },
+  computed: {
+    lazyEnabled() {
+      if ('loading' in HTMLImageElement.prototype) {
+        return 'lazy'
+      } else {
+        return 'auto'
+      }
+    }
+  }
+}
+</script>
---
+++ components/sections/Header.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap md:flex-no-wrap p-6">
+    <div class="flex items-center flex-shrink-0 mr-6">
+      <span class="pr-4">LOGO!</span>
+      <span class="font-semibold text-xl tracking-tight">
+        Blackhawk Force
+      </span>
+    </div>
+    <div class="block md:hidden">
+      <button
+        @click="mobileNavbarToggle"
+        class="flex items-center px-3 py-2 border rounded"
+      >
+        <svg
+          class="fill-current h-3 w-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
+    </div>
+    <div
+      :class="{ hidden: mobileNavbarHidden }"
+      class="w-full block flex-grow md:flex md:items-center md:justify-end"
+    >
+      <div
+        class="text-sm md:text-base md:mr-4"
+        v-for="links in ['Home', 'Services']"
+      >
+        <a
+          class="block mt-4 lg:inline-block md:mt-0 mr-4"
+          href="#responsive-header"
+        >
+          {{ links }}
+        </a>
+      </div>
+      <div>
+        <a
+          class="inline-block text-sm px-4 py-2 leading-none border rounded mt-4 md:mt-0"
+          href="#"
+          >Company Profile</a
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      mobileNavbarHidden: true
+    }
+  },
+  methods: {
+    mobileNavbarToggle() {
+      this.mobileNavbarHidden = !this.mobileNavbarHidden
+    }
+  }
+}
+</script>
