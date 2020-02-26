3d848e0b56f25127322ba2f2106f76feb6a86509
aaron <aaron@mindvalley.com>
2020-02-26 23:07:59 +0800

    shows only 6 logos on smaller devices

--- components/sections/OurClients.vue
+++ components/sections/OurClients.vue
@@ -1,16 +1,16 @@
 <template>
-  <section class="text-white mx-auto py-12 bg-black">
+  <section class="text-white mx-auto py-12">
     <div class="container mx-auto px-4">
       <div class="flex flex-wrap justify-center text-center mb-24">
         <div class="w-full lg:w-6/12 px-4">
-          <h2 class="text-4xl font-semibold">{{ header }}</h2>
+          <h2 class="text-4xl font-semibold text-black">{{ header }}</h2>
           <p class="text-lg leading-relaxed m-4 text-gray-600">
             ...
           </p>
         </div>
       </div>
     </div>
-    <div class="grid grid-cols-4 mx-auto container mb-8" style="line-height: 0;">
+    <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-1 mx-8 mb-8" style="line-height: 0;">
       <div v-for="(client, index) in clients" :key="index" class="w-full h-full bg-white" :class="client.classes">
         <BaseImg :src="client.logo" class="mx-auto"/>
       </div>
